b8a5ae3f29b364aad52b8bd07ee588d302b571e3
pimo <pimo@MacBook-Pro-di-pimo.local>
2013-11-10 18:02:16 +0100

    8

--- loraefesta/static/js/home.js
+++ loraefesta/static/js/home.js
@@ -1,31 +1,39 @@
 jQuery(document).ready(function($) {
 	
 //init
-$('.menu').css('opacity', 0);
-
+$('.menu').css({'opacity': 0, 'margin-top': -10});
 		
 //ROLLOVER
-
 	$('.sezione').mouseenter(function() {
 	var padre =	$(this).attr("ref");;
-	$('.bg.'+padre).animate({
-		height: '110%',
-		width: '110%',
-		marginLeft: '-5%',
-		marginTop: '-5%',
+	$('.bg.'+padre).stop(true, true).animate({
 		opacity: 0
-		}, 400, function() {
-			
-		});
-	$('.menu.'+padre).animate({
-		opacity: 1
 		}, 500, function() {
-			
+			//
 		});
-		
-		
+	$('.menu.'+padre).stop(true, true).animate({
+		opacity: 1,
+		marginTop: 0
+		}, 500, function() {
+			//
+		});
 	});
 
+//ROLLOUT
+	$('.sezione').mouseleave(function() {
+	var padre =	$(this).attr("ref");;
+	$('.bg.'+padre).stop(true, true).animate({
+		opacity: 1
+		}, 400, function() {
+			//
+		});
+	$('.menu.'+padre).stop(true, true).animate({
+		opacity: 0,
+		marginTop: -10
+		}, 500, function() {
+			//
+		});
+	});
 
 
 });
