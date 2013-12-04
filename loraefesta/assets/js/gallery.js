ebcf6b98469b64b519e9f3fffd429c3dea2a94a6
pimo <pimo@MacBook-Pro-di-pimo.local>
2013-12-04 17:38:52 +0100

    arimercules

--- loraefesta/assets/js/gallery.js
+++ loraefesta/assets/js/gallery.js
@@ -22,7 +22,16 @@ jQuery(document).ready(function($) {
 		gallery: {
 		  enabled: true
 		},
-		type: 'image' // this is a default type
+		type: 'image', // this is a default type
+		
+		
+			 // Delay in milliseconds before popup is removed
+	  removalDelay: 300,
+	
+	  // Class that is added to popup wrapper and background
+	  // make it unique to apply your CSS animations just to this exact popup
+	  mainClass: 'mfp-fade'
+		
 	});
 
 });
